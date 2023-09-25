84a65657b16c8afde2c82e955e8145ae7a78fdaa
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2023-09-24 23:46:11 -0600

    removed folder meals_clase

--- meals_clase/android/app/src/main/kotlin/com/example/meals_clase/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.meals_clase
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- meals_clase/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'home_page.dart';
-
-void main() {
-  runApp(MaterialApp(home: MyApp()));
-}
