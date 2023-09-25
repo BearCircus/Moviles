0a180c6265c58bbc2e7388d9f2f968e5ca946051
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2023-09-24 23:36:08 -0600

    animaciones chidas

--- meals/lib/screens/meals_list.dart
+++ meals/lib/screens/meals_list.dart
@@ -17,9 +17,22 @@ class MealsList extends StatelessWidget {
               onTap: () {
                 Navigator.push(
                     context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            MealInfo(meal: mealsList[index])));
+                    PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            MealInfo(meal: mealsList[index]),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(1.0, 0);
+                          const end = Offset.zero;
+                          const curve = Curves.easeInOut;
+
+                          var tween = Tween(begin: begin, end: end)
+                              .chain(CurveTween(curve: curve));
+                          var offsetAnimation = animation.drive(tween);
+
+                          return SlideTransition(
+                              position: offsetAnimation, child: child);
+                        }));
               },
               child: Card(
                 clipBehavior: Clip.antiAliasWithSaveLayer,
